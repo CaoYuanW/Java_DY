09401d82b50ea28c65a5d6c0af89072cf72fc530
CaoYuan <1667854547@qq.com>
2022-11-09 23:44:47 +0800

    Two

--- src/main/java/day02/TestVariable.java
+++ src/main/java/day02/TestVariable.java
@@ -2,6 +2,26 @@ package day02;
 
 public class TestVariable {
     public static void main(String[] args) {
+        int a = 10;
+        a = 20;
+        System.out.println(a);
+
+        double b = 3.14;
+        System.out.println(b);
+
+
+        char c = 'a';
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
+
+        String s = "abc";
+        System.out.println(s);
+
+        int x;
+        x = 100;
+        System.out.println(x);
 
     }
 }
