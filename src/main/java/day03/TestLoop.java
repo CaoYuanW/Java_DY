8b7184ca7aea31fcd16aef84630d3542991ed0d2
CaoYuan <1667854547@qq.com>
2022-11-12 22:38:47 +0800

    Three

--- src/main/java/day03/TestLoop.java
+++ src/main/java/day03/TestLoop.java
@@ -20,17 +20,23 @@ public class TestLoop {
         } while (num < 30);
 
         for (int i = 0; i < 10; i++) {
-            if (i == 5){
+            if (i == 5) {
                 break;
             }
             System.out.println(i);
         }
 
         for (int i = 0; i < 10; i++) {
-            if (i == 5){
+            if (i == 5) {
                 continue;
             }
             System.out.println(i);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("i=" + i + ",j=" + j);
+            }
         }
 
     }
