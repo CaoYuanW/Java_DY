8209d626f1fdd5da6d6903b93cfa3b221dbec1a7
CaoYuan <1667854547@qq.com>
2022-11-12 14:39:18 +0800

    Two

---
+++ src/main/java/day02/TestDataType.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class TestDataType {
+    public static void main(String[] args) {
+        byte b = 10 + 20;
+        System.out.println(b);
+
+        byte b1 = 10;
+        //byte B2 = b1 + 20;  异常，类型不匹配
+
+
+
+        double d = 10;
+        System.out.println(d);
+
+
+        int a = 50;
+        short s = (short) a;
+        System.out.println(s);
+    }
+}
