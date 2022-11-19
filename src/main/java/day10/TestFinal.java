e446311fe03d026462ef7e1ca6a657908cb68ed0
CaoYuan <1667854547@qq.com>
2022-11-19 14:53:59 +0800

    Ten

---
+++ src/main/java/day10/TestFinal.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class TestFinal {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+final class Animal {
+    private final String name = "animal";
+
+    public final void test() {
+        System.out.println("test");
+    }
+}
