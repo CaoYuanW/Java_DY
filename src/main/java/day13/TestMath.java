6221e5b08c544a90c47f94f4b0857234ccdfe164
CaoYuan <1667854547@qq.com>
2022-11-20 21:01:00 +0800

    Thirteen

---
+++ src/main/java/day13/TestMath.java
@@ -0,0 +1,13 @@
+package day13;
+
+public class TestMath {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.ceil(10.1));
+        System.out.println(Math.floor(10.1));
+        System.out.println(Math.round(10.1));
+        System.out.println(Math.pow(10,3));
+        System.out.println(Math.random());
+    }
+}
